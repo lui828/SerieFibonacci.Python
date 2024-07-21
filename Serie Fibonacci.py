d74800761aec2ed60de44c19f7b71a0b47d2c511
luis <luis.davila@tecnologicoliceocristiano.edu.ec>
2024-07-21 15:44:58 -0500

    ejercicio en clases

---
+++ Serie Fibonacci.py
@@ -0,0 +1,12 @@
+# Realice un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+#  * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+#  * 0, 1, 1, 2, 3, 5, 8, 13...
+
+N = 0
+A = 1
+C=0
+for C in range (50):
+    print(N)
+    S=N+A
+    N=A
+    A=S
